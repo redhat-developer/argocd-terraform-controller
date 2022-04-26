debfa245b52651fd5a06e0c5220ccfa0814e5d71
Christian Hernandez <christianh814@users.noreply.github.com>
2022-04-26 15:29:30 -0700

    Create initial GVK (#11)
    * Create initial GVK
    
    Signed-off-by: Christian Hernandez <chernand@redhat.com>
    
    * no group
    
    Signed-off-by: Christian Hernandez <chernand@redhat.com>

---
+++ api/v1alpha1/terraform_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TerraformSpec defines the desired state of Terraform
+type TerraformSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Terraform. Edit terraform_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// TerraformStatus defines the observed state of Terraform
+type TerraformStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Terraform is the Schema for the terraforms API
+type Terraform struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TerraformSpec   `json:"spec,omitempty"`
+	Status TerraformStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TerraformList contains a list of Terraform
+type TerraformList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Terraform `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Terraform{}, &TerraformList{})
+}
