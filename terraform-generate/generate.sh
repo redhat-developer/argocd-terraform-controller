78d7bccb236f132ccddae10d68153cf5b68f103b
Joseph Sawaya <jsawaya@redhat.com>
2022-08-09 16:47:23 -0400

    wip
    Signed-off-by: Joseph Sawaya <jsawaya@redhat.com>

--- terraform-generate/generate.sh
+++ terraform-generate/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
 kubectl create configmap -n $ARGOCD_APP_NAMESPACE $ARGOCD_APP_NAME-terraform --from-file=$ARGOCD_APP_SOURCE_PATH -o json --dry-run
-echo "{\"apiVersion\": \"argoproj.io/v1alpha1\", \"kind\": \"Terraform\", \"metadata\": {\"name\": \"$ARGOCD_APP_NAME\", \"namespace\": \"$ARGOCD_APP_NAMESPACE\"}, \"spec\": {\"revision\": \"$ARGOCD_APP_REVISION\", \"completed\": false}}"
+echo "{\"apiVersion\": \"argoproj.io/v1alpha1\", \"kind\": \"Terraform\", \"metadata\": {\"name\": \"$ARGOCD_APP_NAME\", \"namespace\": \"$ARGOCD_APP_NAMESPACE\"}, \"spec\": {\"revision\": \"$ARGOCD_APP_REVISION\"}}"
