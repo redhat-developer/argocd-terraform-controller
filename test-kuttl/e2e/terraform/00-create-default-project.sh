3af89836fa8c87c438b9950d00e84bec76fef037
Ryan Cook <rcook@redhat.com>
2022-08-16 13:44:09 -0400

    test against new functionality
    Signed-off-by: Ryan Cook <rcook@redhat.com>

---
+++ test-kuttl/e2e/terraform/00-create-default-project.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+set -e -o pipefail
+
+kubectl apply -n argocd -f - <<EOF
+---
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: default
+  namespace: argocd
+spec:
+  sourceRepos:
+    - '*'
+  destinations:
+    - namespace: '*'
+      server: '*'
+  clusterResourceWhitelist:
+    - group: '*'
+      kind: '*'
+EOF
