d792a4e95b45442eadfcf3e6e32ba37fac23e6fd
Ryan Cook <rcook@redhat.com>
2022-10-17 15:53:36 -0400

    reset file
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- test-kuttl/e2e/terraform/05-create-terraform.sh
+++ test-kuttl/e2e/terraform/05-create-terraform.sh
@@ -6,7 +6,7 @@ kubectl apply -n argocd -f - <<EOF
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: jo-terraform-test
+  name: terraform-test
 spec:
   destination:
     namespace: argocd
@@ -15,8 +15,8 @@ spec:
     automated: {}
   project: default
   source:
-    path: manifests
+    path: config/samples
     plugin:
       name: argocd-terraform-generator
-    repoURL: https://github.com/josephsawaya/terraform-test.git
+    repoURL: https://github.com/sabre1041/argocd-terraform-controller.git
 EOF
